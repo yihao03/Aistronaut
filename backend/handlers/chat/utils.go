2cf8b798fe3bf0a8d1e1c407cb617ba3db7d5b35
yihao <yihao039@gmail.com>
2025-09-21 16:15:05 +0800

    update requirement

--- backend/handlers/chat/utils.go
+++ backend/handlers/chat/utils.go
@@ -3,6 +3,7 @@ package chat
 import (
 	"reflect"
 
+	"github.com/yihao03/Aistronaut/m/v2/db"
 	"github.com/yihao03/Aistronaut/m/v2/models"
 )
 
@@ -10,10 +11,25 @@ func CheckDetailsComplete(trip *models.Trip) bool {
 	v := reflect.ValueOf(trip).Elem() // Use .Elem() to dereference the pointer
 	done := true
 	for i := 0; i < v.NumField(); i++ {
+		fieldName := v.Type().Field(i).Name
+		if fieldName == "TripID" || fieldName == "UserID" {
+			continue
+		}
 		if v.Field(i).IsZero() {
 			done = false
 		}
 	}
 
 	return done
+}
+
+func HasFlightDetails(trip *models.Trip) bool {
+	db := db.GetDB()
+
+	booking := models.FlightBookings{}
+	if err := db.Find(booking, "trip_id = ?", trip.TripID).Error; err != nil {
+		return false
+	}
+
+	return booking.BookingID != ""
 }
